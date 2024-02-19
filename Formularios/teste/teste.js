3c7879b21e35911769bf1f2a563767bd20d27100
Diel Batista <124736598+dielbatista@users.noreply.github.com>
2024-02-18 22:43:34 -0300

    v
    v

---
+++ Formularios/teste/teste.js
@@ -0,0 +1,15 @@
+let n1 = 5
+let n2 = 3
+let n3 = n1/n2 
+
+console.log(n3)
+
+
+
+// Dicas para criar um algoritmo:
+
+//     Descubra qual o objetivo (problema).
+//     Comece a se questionar como resolver esse problema.
+//     Responda as perguntas que forem surgindo até não ter mais nenhuma.
+//     Baseado nas perguntas e respostas crie os passos (algoritmo) até chegar no objetivo ou resolver o problema.
+
