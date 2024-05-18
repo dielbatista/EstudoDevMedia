869fa4d467401d662a1dd7b9dac3e5a368710d06
dielbatista <dielxlem229@gmail.com>
2024-05-18 11:59:15 -0300

    Adding new files

---
+++ Estudo-Dev-Media-JS/if_ternario2.js
@@ -0,0 +1,10 @@
+// let senha = "abc1"
+
+// let result = (senha.length >= 6) ? "senha Forte" : "Senha fraca" ;
+// console.log(result)
+
+
+var numer_1 = 10
+var numero_2 = 6
+var numero_3 = 4
+console.log(++numer_1 <= (numero_3 + numero_2))
