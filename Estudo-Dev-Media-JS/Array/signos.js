be9d96728b4a5c79106a3a76eaf7c16fd6a15a5a
dielbatista <dielxlem229@gmail.com>
2025-02-06 20:11:16 -0300

    Array-Signos

---
+++ Estudo-Dev-Media-JS/Array/signos.js
@@ -0,0 +1,7 @@
+var signos = [
+    {signo1: "lorem1", descricao: "lorem2"},
+    {signo2: "lorem3", descricao: "lorem4"},
+    {signo3: "lorem4", descricao: "lorem5"}
+]
+
+console.log(signos[2].descricao)
