e955aa2887c5e8baf95dbe9ca1c5ba4200833395
dielbatista <dielxlem229@gmail.com>
2025-02-12 20:47:41 -0300

    c°, f°.js

---
+++ Inputs/celciusFahrenheit.js
@@ -0,0 +1,8 @@
+const entradaDados = require('readline-sync')
+console.log("Conversor C° - F°")
+
+let celcius = entradaDados.question("Informe o Valor em Celcius: ")
+//let fahrenheit = entradaDados.question("Informe o Valor em Fahrenheit: ")
+let resultado = Number(celcius)
+resultado = Number(celcius * 1.8) + 32
+console.log(resultado)
