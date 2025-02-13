e92f6580bbc1278910113615d6a72d5f6dbfd4b7
dielbatista <dielxlem229@gmail.com>
2025-02-13 16:19:07 -0300

    C° F° Modification

--- Inputs/celciusFahrenheit.js
+++ Inputs/celciusFahrenheit.js
@@ -4,5 +4,5 @@ console.log("Conversor C° - F°")
 let celcius = entradaDados.question("Informe o Valor em Celcius: ")
 //let fahrenheit = entradaDados.question("Informe o Valor em Fahrenheit: ")
 let resultado = Number(celcius)
-resultado = Number(celcius * 1.8) + 32
-console.log(resultado)
+resultado = Number(celcius * 1.8) + 32  
+console.log(resultado.toFixed(2))
