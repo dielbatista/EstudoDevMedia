bc2fb8e831fb917e4935343e2a6b0043066b3c05
dielbatista <dielxlem229@gmail.com>
2025-02-13 16:51:53 -0300

    Projeto Calcular Juros

---
+++ Estudo-Dev-Media-JS/ProjetoJuros/juros.js
@@ -0,0 +1,5 @@
+const entradaDados = require("readline-sync");
+
+let valorDivida = entradaDados.question("Informe Qual o valor Devido?: ")
+let diasAtraso = entradaDados.question("Informe quantos dias passaram desde o vencimento do boleto: ")
+let taxaJuros = entradaDados.question("Informe a Taxa de Juros: ")
