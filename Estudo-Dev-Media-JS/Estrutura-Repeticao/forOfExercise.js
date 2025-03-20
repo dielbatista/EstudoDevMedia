b258f9f6b86a04668d80e4af186cbc811cc71f1a
dielbatista <dielxlem229@gmail.com>
2025-03-20 08:52:06 -0300

    Manipulacao de Strings

---
+++ Estudo-Dev-Media-JS/Estrutura-Repeticao/forOfExercise.js
@@ -0,0 +1,6 @@
+let idiomas = "Portugues,Ingles,frances,espanhol";
+let array_idiomas = idiomas.split(",");
+
+for (let idioma of array_idiomas) {
+  console.log("Idiomas: " + idioma);
+}
---
+++ Estudo-Dev-Media-JS/Manipulacao_strings/replace1.js
@@ -0,0 +1,11 @@
+let frase = "A Linguagem PHP é muito Boa, por isso eu Amo PHP";
+
+let frase_corrigida = frase.replace("PHP", "Javascript");
+// console.log(frase_corrigida);
+
+let nome = "Ana é Especialista em Python";
+
+nome.replace("Python", "Javascript");
+nome.replace("Ana", "Carla");
+let resultado = nome.substring(4, 18);
+console.log(resultado);
